f0aeabf964373789c37f09cdbe37b85739d57af3
17031moonyoung <zipkero@ecounterp.co.kr>
2021-05-18 08:56:44 +0900

    Label change functional component

--- front/src/components/control/Label.js
+++ front/src/components/control/Label.js
@@ -1,19 +1,15 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { Text } from "@blueprintjs/core";
 
-export default class Label extends PureComponent {
-  constructor(props) {
-    super(props);
+const Label = (props) => {
+  const { values = [], formatter } = props;
+  let value = values.length > 0 ? values[0].toString() : "";
+
+  if (typeof formatter === "function" && values.length > 0) {
+    value = formatter(value);
   }
 
-  render() {
-    const { values = [], formatter } = this.props;
-    let value = values.length > 0 ? values[0].toString() : "";
+  return <Text ellipsize={true}>{value}</Text>;
+};
 
-    if (typeof formatter === "function" && values.length > 0) {
-      value = formatter(value);
-    }
-
-    return <Text ellipsize={true}>{value}</Text>;
-  }
-}
+export default Label;
